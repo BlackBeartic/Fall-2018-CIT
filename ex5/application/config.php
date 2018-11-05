dc84c0995c67da61273eb64dbaed33de372554bc
Patrick Rudolph <patrudol@iu.edu>
2018-11-04 20:27:03 -0500

    Ex5
    Exercise 5

---
+++ ex5/application/config.php
@@ -0,0 +1,12 @@
+<?php
+define('DEFAULT_VIEW', 'home');//set this to any page to be the default home page
+define('BASE_URL', 'http://corsair.cs.iupui.edu:23711/ex5/');
+
+//database info
+define('DB_HOST', 'localhost');
+define('DB_USER', 'patrudol');
+define('DB_PASS', '1705');
+define('DB_NAME', 'patrudol_db');
+
+define('REQFIELD', '<font color="#FF0000">*</font>');
+?>
